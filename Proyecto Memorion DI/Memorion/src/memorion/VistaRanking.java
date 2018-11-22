77bccd574738237c9649c0c1465f9dbcd0cef0f6
SergioGD7 <45030664+SergioGD7@users.noreply.github.com>
2018-11-22 19:53:11 +0100

    Update VistaRanking.java

--- Proyecto Memorion DI/Memorion/src/memorion/VistaRanking.java
+++ Proyecto Memorion DI/Memorion/src/memorion/VistaRanking.java
@@ -5,6 +5,8 @@
  */
 package memorion;
 
+import java.awt.Image;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 /**
@@ -14,6 +16,23 @@ import javax.swing.JFrame;
 public class VistaRanking extends JFrame{
     
     public VistaRanking(){
+        crearInterfaz();
+    }
+    
+    public void crearInterfaz(){
+        setTitle("Ranking");
+
+        //Cambiamos el icono de la aplicación
+        Image icon = new ImageIcon(getClass().getResource("trasera.png")).getImage();
+        setIconImage(icon);
+        
+        setLayout(null);
+       
+        setResizable(false);
+        setSize(1366,768);
         setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        //Para que la ventana salga centrada
+        setLocationRelativeTo(null);
     }
 }
